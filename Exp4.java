56ba1e3bc85cff6f6a2dd775a4293d3bba3e0147
charanjeetgaba <136601406+charanjeetgaba@users.noreply.github.com>
2025-02-19 10:15:16 +0530

    Update Exp4.java

--- Exp4.java
+++ Exp4.java
@@ -48,3 +48,119 @@ public class Exp4E {
     // ID: 2, Name: Atul, Salary: 60000.0
 
 //MEDIUM
+import java.util.*;
+
+class Card {
+    private String symbol, value;
+
+    public Card(String symbol, String value) {
+        this.symbol = symbol;
+        this.value = value;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    @Override
+    public String toString() {
+        return value + " of " + symbol;
+    }
+}
+
+class CardCollection {
+    private Collection<Card> cards = new ArrayList<>();
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public void displayCardsBySymbol(String symbol) {
+        cards.stream().filter(c -> c.getSymbol().equalsIgnoreCase(symbol)).forEach(System.out::println);
+    }
+}
+
+public class Exp4M {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        CardCollection collection = new CardCollection();
+
+        collection.addCard(new Card("Hearts", "Ace"));
+        collection.addCard(new Card("Spades", "King"));
+
+        System.out.print("Enter a symbol to search: ");
+        String symbol = scanner.nextLine();
+
+        collection.displayCardsBySymbol(symbol);
+        scanner.close();
+    }
+}
+//OUTPUT
+        // Enter a symbol to search: hearts
+        // Ace of Hearts
+
+//HARD
+import java.util.concurrent.locks.*;
+
+class TicketBookingSystem {
+    private boolean[] seats;
+    private Lock lock;
+
+    public TicketBookingSystem(int seatCount) {
+        seats = new boolean[seatCount];
+        lock = new ReentrantLock();
+    }
+
+    public void bookSeat(int seatNumber, String customer) {
+        lock.lock();
+        try {
+            if (!seats[seatNumber]) {
+                seats[seatNumber] = true;
+                System.out.println(customer + " successfully booked seat " + seatNumber);
+            } else {
+                System.out.println("Seat " + seatNumber + " is already booked.");
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+}
+
+class BookingThread extends Thread {
+    private TicketBookingSystem system;
+    private int seatNumber;
+    private String customer;
+
+    public BookingThread(TicketBookingSystem system, int seatNumber, String customer, int priority) {
+        this.system = system;
+        this.seatNumber = seatNumber;
+        this.customer = customer;
+        setPriority(priority);
+    }
+
+    @Override
+    public void run() {
+        system.bookSeat(seatNumber, customer);
+    }
+}
+
+public class Exp4H {
+    public static void main(String[] args) {
+        TicketBookingSystem system = new TicketBookingSystem(10);
+        
+        Thread vip1 = new BookingThread(system, 3, "VIP_1", Thread.MAX_PRIORITY);
+        Thread vip2 = new BookingThread(system, 3, "VIP_2", Thread.MAX_PRIORITY);
+        Thread user1 = new BookingThread(system, 3, "User_1", Thread.MIN_PRIORITY);
+        Thread user2 = new BookingThread(system, 3, "User_2", Thread.NORM_PRIORITY);
+        
+        vip1.start();
+        vip2.start();
+        user1.start();
+        user2.start();
+    }
+}
+//OUTPUT
+        // VIP_1 successfully booked seat 3
+        // Seat 3 is already booked.
+        // Seat 3 is already booked.
+        // Seat 3 is already booked.
